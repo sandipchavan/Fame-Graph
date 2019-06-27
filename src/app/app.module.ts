50316b71934946a0f01e591e491da3a096ac08ff
sandipchavan <sandipchavan1994@gmail.com>
2019-06-28 00:20:05 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/facade/header/header.component';
+import { NavbarComponent } from './components/facade/navbar/navbar.component';
+import { FooterComponent } from './components/facade/footer/footer.component';
+import { LoginComponent } from './components/facade/login/login.component';
+import { SignupComponent } from './components/facade/signup/signup.component';
+import { UserProfileComponent } from './components/feeds/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
